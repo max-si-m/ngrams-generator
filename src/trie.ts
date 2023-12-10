a428f1c9857c6dfc14c17c68499926140429685f
Maxim Dzhulii <mak7.dj@gmail.com>
2023-12-10 10:07:47 +0200

    Add method to generate combinations

--- src/trie.ts
+++ src/trie.ts
@@ -1,16 +1,21 @@
-type TreeNode = {
-  val?: string;
-  parent?: TreeNode; // head
-  children: TreeNode[];
+type TrieNode = {
+  val: string;
+  parent?: TrieNode; // head
+  children: TrieNode[];
   freq: number;
   isWord: boolean;
 }
 
+type CombinationResult = {
+  combination: string,
+  freq: number
+}
+
 export default class Trie {
-  root: TreeNode
+  root: TrieNode
 
   constructor() {
-    this.root = { val: undefined, children: [], freq: 0, isWord: false }
+    this.root = { val: '', children: [], freq: 0, isWord: false }
   }
 
   addWord(word: string) {
@@ -40,9 +45,49 @@ export default class Trie {
     this.updateFreq(root)
   }
 
-  findChildrenNode(node: TreeNode, letter: string): TreeNode | undefined {
+  findChildrenNode(node: TrieNode, letter: string): TrieNode | undefined {
     // TODO: check performace if use Array(26) and get node directly by index
     return node.children.find(n => n.val === letter);
+  }
+
+  // TODO: this do not work, no sliding windown, should fix, but lets come back later to this
+  generateCombinations(targetLength: number, resultMap: Map<string, number>): void {
+    function dfs(node: TrieNode, currentSubstring: TrieNode[]) {
+      if (!node) return;
+
+      if (currentSubstring.length === targetLength) {
+        const res = buildResultStringWithFreq(currentSubstring);
+        const freq = resultMap.get(res.combination) || 0;
+        resultMap.set(res.combination, freq + res.freq); // but this should in revers order Map<freq, string>
+
+        currentSubstring.splice(0, 1);
+      }
+
+      for (let i = 0; i < node.children.length; i++) {
+        const childNode = node.children[i];
+        if (childNode) {
+          currentSubstring.push(childNode);
+          dfs(childNode, currentSubstring);
+        }
+
+        currentSubstring.pop()
+      }
+    }
+
+    dfs(this.root, []);
+
+    // maybe extract to separate function
+    function buildResultStringWithFreq(tmpRes: TrieNode[]): CombinationResult {
+      const res: string[] = new Array(tmpRes.length)
+      let freq: number = 0
+
+      for(let i = 0; i < tmpRes.length; i++) {
+        res.push(tmpRes[i].val!)
+        freq += tmpRes[i].freq
+      }
+
+      return { combination: res.join(''), freq: freq }
+    }
   }
 
   // Remove word from the trie
@@ -90,7 +135,7 @@ export default class Trie {
     return node?.isWord
   }
 
-  private updateFreq(node: TreeNode): void {
+  private updateFreq(node: TrieNode): void {
     node.freq += 1
 
     if (node.parent)
@@ -105,7 +150,7 @@ export default class Trie {
     }
   }
 
-  private printNode(node: TreeNode, level: number): void {
+  private printNode(node: TrieNode, level: number): void {
     if (node) {
       const indent = ' '.repeat(level * 2);
       console.log(`${indent}${node.val}${node.isWord ? '*' : '' }:${node.freq}`);
@@ -115,3 +160,19 @@ export default class Trie {
     }
   }
 }
+
+// const trie = new Trie()
+//
+// trie.addWord('cat')
+// trie.addWord('cats')
+// trie.addWord('car')
+// trie.addWord('crane')
+// trie.addWord('cartoon')
+// trie.addWord('cool')
+//
+// trie.printTree()
+// // lets do this in simple way
+// let resMap: Map<string, number> = new Map()
+// trie.generateCombinations(2, resMap)
+//
+// console.log(resMap)
