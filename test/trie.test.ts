911411590b76eb86a8ec13879c855bd17bc50010
Maxim Dzhulii <mak7.dj@gmail.com>
2023-12-25 21:34:49 +0200

    fix: add proper test case for generateCombinations

--- test/trie.test.ts
+++ test/trie.test.ts
@@ -9,3 +9,18 @@ test('Trie#addWord', () => {
   expect(hNode.freq).toBe(1)
   expect(hNode.children.length).toBe(1)
 })
+
+
+test('Trie#', () => {
+  const trie = new Trie()
+  trie.addWord('cat')
+  trie.addWord('cats')
+  trie.addWord('car')
+  trie.addWord('care')
+  trie.addWord('cool')
+
+  const resultMap: Map<string, number> = new Map()
+  trie.generateCombinations(2, resultMap)
+
+  expect(Array.from(resultMap.keys())).toEqual(['ca', 'co', 'at', 'ar', 'ts', 're', 'oo', 'ol'])
+})
